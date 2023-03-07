fe6d23b52504f3a38205902839e765774b6ddeec
Matthew Green <greenmatthew07@gmail.com>
2023-03-06 22:07:16 -0600

    Fixed spelling, grammar, and clarity of server computer webpage.

--- www/server_computer.php
+++ www/server_computer.php
@@ -7,20 +7,11 @@
 
 <body>
     <div class="container">
-        <center>
-            <h3 id="server_computer">
-                <a href="https://www.matthewgreen.gg/server_computer.php">Server Computer</a>
-            </h3>
-            <br>
-            <div class="container">
-                <figure id="preview">
-                    <img src="server_computer/server_computer.jpg" alt="Server Computer Preview"
-                        style="width:300px;height:400px;">
-                </figure>
-            </div>
-
-        </center>
-
+        <h3 id="server_computer">Server Computer</h3>
+        <br>
+        <figure id="preview">
+            <img class="auto_fit" src="server_computer/server_computer.jpg" alt="Server Computer Preview">
+        </figure>
         <br>
 
         <div>
@@ -44,18 +35,19 @@
             <h3 id="summary">Summary <a href="#summary">#</a></h3>
             <p>
                 A hobby of mine as of lately is working on an old server computer that I bought. At first, I just wanted
-                a dedicated computer for hosting video game servers. Next thing I know I am buying a server rack for it,
-                trying different operating systems for my use case, and seeing infinite possibilities of self-hosting
-                with Docker containers. I can say that I am now a huge fan of self-hosting and Docker containers. I am
-                even hosting this website off it and many other services. As well as setting up security with SSL
-                certificates and solving the issue of having a dynamic public IP address for these services.
+                a dedicated computer for hosting video game servers for me and my friends. Next thing I know I am buying
+                a server rack for it, trying different operating systems for my use case, and seeing the infinite
+                possibilities of self-hosting with Docker containers. I can say that I am now a huge fan of self-hosting
+                and Docker containers. I am even hosting this website off it and several other services. As well as
+                upgrading security with SSL certificates and solving the issue of having a dynamic public IP address for
+                these services.
             </p>
             <p>
                 In total I am self-hosting with Docker containers a portfolio website, git website, cloud storage, media
-                streaming service, JupyterLabs service, and various game server whenever they're needed. All accessible
-                by me or anyone else in the world, whose been giving access, using my domain name with its respective
-                subdomain and an associated wildcard SSL certificate to ensure privacy of my data. I am sure this list
-                will grow over time as I find new services that I need in my life.
+                streaming service, JupyterLabs service, and a game server or two occasionally. All accessible by me or
+                anyone else in the world, whose been giving access, using my domain name with the service's respective
+                subdomain and an associated wildcard SSL certificate to ensure privacy of data. I am sure this list will
+                grow over time as I find new services that I need in my life.
             </p>
         </div>
 
@@ -66,18 +58,24 @@
                 experience, education, and tells you more about myself.
             </p>
             <p>
-                I started with a little knowledge about HTML and not much else about hosting a website. I started with
-                style templates. After finding one I liked, I got to work writing HTML and adding organization and
-                content. I found static webpages workflow was working against me and the workflow I was so use to in
-                programming. So I tried to change everything to dynamic webpages, but ran into some issues trying to do
-                that with just HTML. I then switched to using PHP to better suit my needs. While I nearly use no PHP the
-                biggest thing was the ability to break the website down into different modules. Now I had a 'header.php'
-                and a 'footer.php' files and so on. Then I could include these inside other PHP files to remove code
-                duplication, just like I would in a program. Thus, making it much easier to update common elements of
-                different webpages. I also used Git to backup my work and you can see the source code on my personal Git
-                website <a href="https://git.matthewgreen.gg/matthewgreen/matthewgreen.gg">here</a> or on GitHub <a
-                    href="https://github.com/greenmatthew/matthewgreen.gg">here</a>.
+                I started with a little knowledge about HTML, CSS, and hosting a website. I started with searching for a
+                style template that I resonated with me. I landed on a style that looked like a terminal of course. I
+                then got to work writing HTML adding content.
+            </p>
+            <p>
+                After a while I found, a static webpage workflow was frustrating to handle with more than one webpage
+                with common elements, so I tried to change everything to dynamic webpages. I ran into some issues trying
+                to do that with just HTML, however, I switched to using a mixture of HTML and PHP to better suit my
+                needs. While I do not use much PHP, it helped to break down the website into different modules and I
+                reuse said modules, to prevent code duplication.
+            </p>
+            <p>
 
+            </p>
+            <p>
+                I used Git, like I do for any project, for safely storing and doing version control of the source code
+                on my personal Git website <a href="https://git.matthewgreen.gg/matthewgreen/matthewgreen.gg">here</a>
+                or on GitHub <a href="https://github.com/greenmatthew/matthewgreen.gg">here</a>.
             </p>
         </div>
 
@@ -92,7 +90,7 @@
             <p>
                 At first, I was having many issues with repositories becoming corrupted or just being missing, but after
                 switching from a SQLite3 database to a MySQL database everything worked great. Now I can use Git just
-                like I would with GitHub, and it has been extremely nice to have.
+                like I would with GitHub, and it has been pleasant to have.
             </p>
             <p>
                 You can see this website and all my public repositories
@@ -103,10 +101,10 @@
         <div>
             <h3 id="wildcard_ssl_cert">Wildcard SSL Certificate <a href="#wildcard_ssl_cert">#</a></h3>
             <p>
-                I have setup a wildcard SSL certificate so all my different subdomains are secure and also to make it
-                easier to add a new and secure subdomains in the future. You can even see this certificate in action.
-                Depending on your browser, you can most likely see a little lock to the left of your address bar at the
-                top. That means that you have a secure connection to the server.
+                I have setup a wildcard SSL certificate, so all my different subdomains are secure with a single
+                certificate. It also is incredibly easy to secure new subdomains in the future. You can even see this
+                certificate in action. Depending on your browser, you can most likely see a little lock to the left of
+                your address bar at the top. That means that you have a secure connection to the server.
             </p>
         </div>
 
@@ -116,10 +114,8 @@
                 One issue that I was always worried about when hosting different things such as my Git website is if my
                 public IP address would change while I was away from home and needed to access one of my services. Well
                 I found a fix, I learned about DDNS services. All you have to do is run an app provided by the DDNS
-                service, but in my case, a Docker container and my public IP address will automatically update if it
-                changes. Then change the DNS records for my subdomains to redirect not to a hardcoded IP address but to
-                my new subdomain from the DDNS service. Now I no longer must worry about my services going offline
-                anymore.
+                service, in my case a Docker container, and the public IP address my domain name resolves to will
+                automatically update if it changes. Now I no longer must worry about my services going offline anymore.
             </p>
         </div>
 
@@ -135,25 +131,25 @@
         <div>
             <h3 id="game_servers">Game Servers <a href="#game_servers">#</a></h3>
             <p>
-                Every once and a while, me and some friends will want to play a multiplayer game and we will want to
-                host our own server. I always volunteer to host because it can be fun to set them up and I have lots of
-                prior experience doing them. Ever sense I got my server computer it has been a whole lot easier, because
-                I can set up a dedicated server that never goes down so anyone can play at any time. It got even easier
-                since I switched to the operating system, Unraid. I just search for a Docker container for the game I
-                want to host, maybe fiddle with a few settings and port forwarding and its already up and running. I can
-                even add a subdomain for the game server and won't need to worry about my public IP changing or having
-                to constantly give out an IP to friends.
+                Every once and a while, me and some friends will want to play a multiplayer game and host our own
+                server. I always volunteer to host because it can be fun to set them up and I have lots of prior
+                experience doing them. Ever since I got my server computer it has been a whole lot easier, because I can
+                set up a dedicated server that never goes down so anyone can play at any time. It got even easier since
+                I switched to the operating system, Unraid. I just search for a Docker container for the game I want to
+                host, maybe fiddle with a few settings and port forwarding and its already up and running. I can even
+                add a subdomain for the game server and won't need to worry about my public IP address changing or
+                having to constantly give out an IP address to friends.
             </p>
         </div>
 
         <div>
             <h3 id="other_services">Other Services <a href="#other_services">#</a></h3>
             <p>
-                As for what else I host, I have my own personal cloud storage with NextCloud. It is basically Google
-                Drive but without a storage cap. It even has its own plugins for online document viewing and editing
-                just like Google Docs and all sorts of other useful plugins. The German government apparently uses
-                NextCloud for documents because it allows them to securely host their own cloud storage. If its good
-                enough for them its good enough for me.
+                As for what else I host, I have my own personal cloud storage with NextCloud. It is my own personal
+                Google Drive without a storage cap and third-party plugins to customize it to my needs. It even has its
+                own plugins for online document viewing and editing just like Google Docs and all sorts of other useful
+                plugins. The German government uses NextCloud for documents because it allows them to securely host
+                their own cloud storage. If it's good enough for them its good enough for me.
             </p>
             <p>
                 I have also a JupyterLabs service for having a handy python interpreter anywhere in the world and for
@@ -183,13 +179,13 @@
                 <li>
                     Build a brand-new custom server computer from scratch. My current server computer is an old Dell
                     PowerEdge R710, which came out all the way back in 2009. I was only 7 or 8 at the time of its
-                    release.
-                    Virtually everything on it could be many times better. While, it still works incredibly well for me
-                    even
-                    with all the services I am running on it. I want something better. What I would like in it.
+                    release. Everything on it could be many times better. While, it still works incredibly well for most
+                    of my use cases, it is slowly running out of memory from all the docker containers. Also, some game
+                    servers require better hardware than what I have. So, I want something better. What I would like to
+                    see in it.
                     <ul>
                         <li>
-                            Discrete GPU for on-the-fly transcoding and hosting game servers.
+                            Discrete GPU for on-the-fly video transcoding and hosting game servers.
                         </li>
 
                         <li>
@@ -204,14 +200,14 @@
                         </li>
 
                         <li>
-                            Better and quieter fans and possibly liquid cooling. The server computer I have is quite
-                            noticably loud.
+                            Better and quieter fans and liquid cooling. The server computer I have is quite noticeably
+                            loud.
                         </li>
 
                         <li>
                             3.5” and 2.5” drive bays, my current server computer only has 2.5” which are not really
-                            used for hard drives any more which seems to be the best storage for my needs, because I
-                            need lots of storage not necessarily the fastest with a 2.5” SSD.
+                            used for hard drives anymore which seems to be the best storage for my needs, because I need
+                            lots of storage not necessarily the fastest with a 2.5” SSD.
                         </li>
 
                         <li>
@@ -225,17 +221,17 @@
                 </li>
 
                 <li>
-                    Maybe getting a JBOD or Just A Bunch of Disks, which is a very basic server rack mountable chassis
-                    that has several drive bays. Expanding the storage on the server computer without having to upgrade
-                    the drives themselves, but just adding more to the storage pool. Then switching to a ZFS file system
-                    for redundancy rather than using RAID with a RAID controller.
+                    Maybe getting a rack mountable JBOD or Just A Bunch of Disks, which is just a chassis that has
+                    several drive bays. Allowing the expansion of storage on the server computer without having to
+                    upgrade the drives themselves. Then switching to a ZFS file system for redundancy rather than using
+                    RAID with a RAID controller.
                 </li>
 
                 <li>
-                    Getting a UPS or uninterruptible power supply so that my server computer can never suddenly shutdown
-                    from power loss. This would prevent file corruption. Also if my server has access to my main
-                    internet connection from my ISP, my hosted services could in theory stay online even if my house is
-                    without power.
+                    Getting a UPS or uninterruptible power supply so that my server computer can never suddenly
+                    shutdown from power loss. This would prevent file corruption and service disruptions. Also, if my
+                    server has access to my main internet connection from my ISP, my services could in theory stay
+                    online even if my house is without power.
                 </li>
 
                 <li>
@@ -245,13 +241,13 @@
                 </li>
 
                 <li>
-                    Add an actual server rack mountable switch and router. Currently I have a switch but its not
+                    Add an actual server rack mountable switch and router. Currently I have a switch, but it is not rack
                     mountable even though I have a rack mounted patch panel.
                 </li>
 
                 <li>
-                    Learning how to make my own custom Docker containers. So, I can really configure a Docker container
-                    when needed.
+                    Learning how to make my own custom Docker containers. So, when the need arises I can configure a
+                    container to my specific needs.
                 </li>
 
                 <li>
